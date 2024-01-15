2e9e2c19dba1a3235c0e0f0c9abb4810b9c8c24b
Purav2712 <ppanchal@flemingcollege.ca>
2024-01-15 13:11:23 -0500

    created script files
    added code that prints line from Zen of python

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("Explicit is better than implicit.")
+print("Now is better than never.")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print("Beautiful is better than ugly.")
+print("Special cases aren't special enough to break the rules.")
